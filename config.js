f88ee63a6aa5de6a041c5a039a996325bbeebf82
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-10-05 10:22:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' };
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your id",
+SESSION_ID: process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "enter mongo url",
+BOT_NUMBER: process.env.BOT_NUMBER || "94711262551",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4y72vl.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, HANSAMAL-MD IS ALIVE...\n\nOWNER: IMALKA HANSAMAL\n\nTHANKS FOR USING HANSAMAL MD\n\n> *© HANSAMAL-MD*/n/n*> ɢɪᴛʜᴜʙ :* github.com/cobrs11/IMALKA-MD",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-BOT_NUMBER: process.env.BOT_NUMBER || "94711262551",
-OWNER_REACT: process.env.OWNER_REACT || "🔆",
+PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-READ_CMD: process.env.READ_CMD || "true",
 };
 
